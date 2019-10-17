6968f6f70ee282cb34aa3f66a6afce76db8768ea
lizamashchenko <55406171+lizamashchenko@users.noreply.github.com>
2019-10-17 18:58:42 +0300

    Add files via upload

---
+++ Labirinth.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirinth
+{
+    class Labirinth
+    {
+        static Random r = new Random();
+        static int height = 20, width = 10, freq = 30, dx, dy, newX, newY;
+        static int CharX = r.Next(1, width), CharY = r.Next(1, height);
+        static int FinishX = r.Next(1, width), FinishY = r.Next(1, height);
+        static char[,] Map = new char[width, height];
+
+
+
+
+
+
+
+        static void Init()
+        {
+            GenerateMap();
+            PlaceCharacter();
+        }
+        static char[,] GenerateMap()
+        {
+            int RandNum;
+            for (int i = 0; i < height; i++) 
+            {
+                for (int j = 0; i < width; j++) 
+                {
+                    RandNum = r.Next(100);
+                    if (RandNum < freq) Map[i, j] = '#';
+                    else Map[i, j] = ' ';
+                }
+            }
+            Map[FinishX, FinishY] = 'F';
+            return Map;
+
+        }
+        static char[,] PlaceCharacter()
+        {
+            Map[CharX, CharY] = '@';
+            return Map;
+        }
+
+
+
+
+
+        static void TryMove()
+        {
+            newX = CharX + dx;
+            newY = CharY + dy;
+            if (CanGoTo() == true) GoTo();
+        }
+        static bool IsWalkable()
+        {
+            return Map[newX, newY] != '#';
+        }
+        static bool CanGoTo()
+        {
+            if (newX > width && newY > height) return false;
+            if (!IsWalkable()) return false;
+            return true;
+        }
+        static void GoTo()
+        {
+            CharX = newX;
+            CharY = newY;
+        }
+        static bool IsEndgame()
+        {
+            if (Map[CharX, CharY] == 'F') return true;
+            return false;
+        }
+        
+
+
+
+
+
+
+
+
+
+
+        static void Draw()
+        {
+            for (int i = 0; i < height; i++) 
+            {
+                for (int j = 0; j < width; j++) 
+                {
+                    if (i == CharX && j == CharY) Map[i, j] = '@';
+                    Console.Write(Map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Input()
+        {
+            dx = 0;
+            dy = 0;
+            switch (Console.ReadKey().Key)
+            {
+                case ConsoleKey.UpArrow:
+                case ConsoleKey.W:
+                    dy = -1;
+                    break;
+                case ConsoleKey.DownArrow:
+                case ConsoleKey.S:
+                    dy = 1;
+                    break;
+                case ConsoleKey.RightArrow:
+                case ConsoleKey.D:
+                    dx = 1;
+                    break;
+                case ConsoleKey.LeftArrow:
+                case ConsoleKey.A:
+                    dx = -1;
+                    break;
+            }
+        }
+        static void Logic()
+        {
+            TryMove();
+            IsEndgame();
+        }
+
+
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Init();
+            while (!IsEndgame())
+            {
+                Draw();
+                Input();
+                Logic();
+            }
+            Console.WriteLine("!!!WIN!!!");
+        }
+    }
+}
---
+++ Tic Tac Toe.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2
+{
+    class Program
+    {
+        static char[,] Map;
+        static int TurnN = 0, newX, newY, x, y;
+        static char winner;
+
+
+
+
+
+
+        static char[,] GenerateMap(char[,] Map)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Map[i, j] = '^';
+                    Console.Write(Map[i, j]);
+                }
+                Console.WriteLine();
+            }
+            return Map;
+        }
+        static bool CanPut(int newX, int newY)
+        {
+            if (Map[newX, newY] != 'X' && Map[newX, newY] != 'Y' && newX >= 0 && newY >= 0) return true;
+            return false;
+        }
+        static void Put()
+        {
+            x = newX;
+            y = newY;
+        }
+        static bool WhoseTurn(int TurnN)
+        {
+            if (TurnN % 2 == 0) return true;
+            else return false;
+        }
+        static void TryPut(int newX, int newY)
+        {
+            if (CanPut(newX, newY)) Put();
+        }
+        static bool IsEndgame()
+        {
+            if (Map[0, 0] == Map[0, 1] && Map[0, 1] == Map[0, 2] && Map[0, 2] == 'X') { winner = '1'; return true; }
+            else if (Map[1, 0] == Map[1, 1] && Map[1, 1] == Map[1, 2] && Map[0, 2] == 'X') { winner = '1'; return true; }
+            else if (Map[2, 0] == Map[2, 1] && Map[2, 1] == Map[2, 2] && Map[2, 2] == 'X') { winner = '1'; return true; }
+            else if (Map[0, 0] == Map[1, 0] && Map[1, 0] == Map[2, 0] && Map[2, 0] == 'X') { winner = '1'; return true; }
+            else if (Map[0, 1] == Map[1, 1] && Map[1, 1] == Map[2, 1] && Map[2, 1] == 'X') { winner = '1'; return true; }
+            else if (Map[0, 2] == Map[1, 2] && Map[1, 2] == Map[2, 2] && Map[2, 2] == 'X') { winner = '1'; return true; }
+            else if (Map[0, 0] == Map[1, 1] && Map[1, 1] == Map[2, 2] && Map[2, 2] == 'X') { winner = '1'; return true; }
+            else if (Map[2, 0] == Map[1, 1] && Map[1, 1] == Map[0, 2] && Map[0, 2] == 'X') { winner = '1'; return true; }
+            else if (Map[0, 0] == Map[0, 1] && Map[0, 1] == Map[0, 2] && Map[0, 2] == 'Y') { winner = '2'; return true; }
+            else if (Map[1, 0] == Map[1, 1] && Map[1, 1] == Map[1, 2] && Map[1, 2] == 'Y') { winner = '2'; return true; }
+            else if (Map[2, 0] == Map[2, 1] && Map[2, 1] == Map[2, 2] && Map[2, 2] == 'Y') { winner = '2'; return true; }
+            else if (Map[0, 0] == Map[1, 0] && Map[1, 0] == Map[2, 0] && Map[2, 0] == 'Y') { winner = '2'; return true; }
+            else if (Map[0, 1] == Map[1, 1] && Map[1, 1] == Map[2, 1] && Map[2, 1] == 'Y') { winner = '2'; return true; }
+            else if (Map[0, 2] == Map[1, 2] && Map[1, 2] == Map[2, 2] && Map[2, 2] == 'Y') { winner = '2'; return true; }
+            else if (Map[0, 0] == Map[1, 1] && Map[1, 1] == Map[2, 2] && Map[2, 2] == 'Y') { winner = '2'; return true; }
+            else if (Map[2, 0] == Map[1, 1] && Map[1, 1] == Map[0, 2] && Map[0, 2] == 'Y') { winner = '2'; return true; }
+            else { winner = 'D'; return false; }
+        }
+        
+
+
+
+
+
+
+
+        static void Draw()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (j == x && i == y)
+                    {
+                        if (WhoseTurn(TurnN) == true) Map[i, j] = 'X';
+                        else Map[i, j] = 'O';
+                    }
+                    Console.Write(Map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Input()
+        {
+            Console.WriteLine("Player " + (TurnN % 2 + 1) + " write coordinates");
+            var input = Console.ReadLine().Split(' ');
+            newX = int.Parse(input[0]);
+            newY = int.Parse(input[1]);
+        }
+        static void Logic()
+        {
+            TryPut(newX, newY);
+            IsEndgame();
+        }
+
+
+
+
+
+
+        static void Main()
+        {
+            Map=new char[3,3];
+            GenerateMap(Map);
+            while (!IsEndgame())
+            {
+                Input();
+                Draw();
+                Logic();
+                TurnN++;
+            }
+            switch (winner)
+            {
+                case '1':
+                    Console.WriteLine("Player 1 wins!!!");
+                    break;
+                case '2':
+                    Console.WriteLine("Player 2 wins!!!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Draw");
+                    break;
+            }
+        }
+    }
+}
